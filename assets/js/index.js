1e880a41e44a9c9459c3682491e2c19049ede654
Mohit Jain <mohitjain@Mohits-Laptop.local>
2024-08-25 20:53:45 -0700

    Added Projects

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,6 +3,6 @@ function submitForm(){
     var lastname = document.getElementById("lastname").value;
     var message = document.getElementById("message").value;
     console.log(firstname);
-    link = "https://mail.google.com/mail/?view=cm&fs=1&to="+"mohitjain.hmu@gmail.com"+"&su=Lets Get Connected&body="+message+"%0D%0A%0D%0ABest%2C%0D%0A"+firstname+" "+lastname;
+    link = "https://mail.google.com/mail/?view=cm&fs=1&to="+"jain07.mohit2000@gmail.com"+"&su=Lets Get Connected&body="+message+"%0D%0A%0D%0ABest%2C%0D%0A"+firstname+" "+lastname;
     document.getElementById("link").href =link
 }
